af9444b413f95540fdd321964e1868c99983ead5
unknown <somyeong60@kookmin.ac.kr>
2021-11-23 23:54:28 +0900

    Change mjp images and Add wallpaper

--- GameUI/gameUI.py
+++ GameUI/gameUI.py
@@ -1,14 +1,14 @@
 from PyQt5.QtCore import Qt, QSize
-from PyQt5 import QtGui
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QGroupBox, QTextEdit
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QGroupBox, QLineEdit
 from PyQt5.QtWidgets import QSizePolicy
 from PyQt5.QtWidgets import QGridLayout, QVBoxLayout
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtGui import QPixmap, QIcon, QPalette, QImage, QBrush
 
 class QPushButton(QPushButton):
-    def __init__(self, text, height, width):
+    def __init__(self, text, height, width, callback):
         super().__init__()
         self.setText(text)
+        self.clicked.connect(callback)
 
         # QPushButton 크기설정
         self.setMaximumHeight(height)
@@ -62,11 +62,20 @@ class Game(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
 
+        # layout 설정
         self.mainLayout = QGridLayout()
         self.row1Layout = QGridLayout()
         self.row2Layout = QGridLayout()
         self.row3Layout = QGridLayout()
         self.setLayout(self.mainLayout)
+
+        # 배경화면 설정
+        # wallpaper = QImage("wallpaper.jpg")
+        # wallpaper.scaled(QSize())
+        # palette = QPalette()
+        # palette.setBrush(10, QBrush(wallpaper))
+        # self.setPalette(palette)
+
 
 
         # row1
@@ -75,13 +84,13 @@ class Game(QWidget):
         self.OffOrDef = QLabelBox(['공격'], 60, 90)
 
         # 현재 상태 표시
-        self.display = QTextEdit(self) # 현재 상태 표시
+        self.display = QLineEdit(self) # 현재 상태 표시
+        self.display.setAlignment(Qt.AlignCenter)
         self.display.setReadOnly(True)
-        self.display.setAlignment(Qt.AlignRight)
-        self.display.setMaximumHeight(90)
+        self.display.setMaximumHeight(100)
 
         # 나가기 버튼
-        self.exit = QPushButton("나가기", 50, 70)
+        self.exit = QPushButton("나가기", 50, 70, self.exitButtonClicked)
 
         # Layout
         self.row1Layout.addWidget(self.OffOrDef, 0, 0)
@@ -119,14 +128,14 @@ class Game(QWidget):
         # row 3
 
         # 묵찌빠 select 버튼
-        self.묵 = self.shapeSelectButton("묵.png", 150, 150)
-        self.찌 = self.shapeSelectButton("찌.png", 150, 150)
-        self.빠 = self.shapeSelectButton("빠.png", 150, 150)
+        self.muk = self.shapeSelectButton("묵.png", 150, 150, self.mukButtonClicked)
+        self.jji = self.shapeSelectButton("찌.png", 150, 150, self.jjiButtonClicked)
+        self.ppa = self.shapeSelectButton("빠.png", 150, 150, self.ppaButtonClicked)
 
         # Layout
-        self.row3Layout.addWidget(self.묵, 0, 0)
-        self.row3Layout.addWidget(self.찌, 0, 1)
-        self.row3Layout.addWidget(self.빠, 0, 2)
+        self.row3Layout.addWidget(self.muk, 0, 0)
+        self.row3Layout.addWidget(self.jji, 0, 1)
+        self.row3Layout.addWidget(self.ppa, 0, 2)
 
         self.mainLayout.addLayout(self.row3Layout, 2, 0)
 
@@ -139,7 +148,7 @@ class Game(QWidget):
     def shapeShowGroupBox(self, title, image, height, width):
         self.showGroupBox = QGroupBox(title, height, width)
         self.vbox = QVBoxLayout()
-        self.pixmapLabel = QPixmapLabel(image, height - 10, width - 10)
+        self.pixmapLabel = QPixmapLabel(image, height - 20, width - 20)
         self.vbox.addWidget(self.pixmapLabel)
         self.showGroupBox.setLayout(self.vbox)
 
@@ -157,22 +166,42 @@ class Game(QWidget):
         return self.groupBoxLayout
 
     # 묵찌빠 선택 button
-    def shapeSelectButton(self, image, height, width):
-        self.button = QPushButton("", height, width)
-        # self.button.setStyleSheet("background-image : url({0});".format(image))
-        self.button.setIcon(QtGui.QIcon(image))
+    def shapeSelectButton(self, image, height, width, buttonClicked):
+        self.button = QPushButton("", height, width, buttonClicked)
+        self.button.setIcon(QIcon(image))
         self.button.setIconSize(QSize(130, 130))
 
         return self.button
 
 
-    def buttonClicked(self):
-
-        if self.display.text() == 'Error!':
-            self.display.setText("")
+    # 나가기 button 클릭했을 때
+    def exitButtonClicked(self):
 
         button = self.sender()
         key = button.text()
+        self.display.setText(key)
+
+    # 묵 button 클릭
+    def mukButtonClicked(self):
+        shape = 0
+
+        button = self.sender()
+        # key = button.text()
+
+    # 찌 button 클릭
+    def jjiButtonClicked(self):
+        shape = 1
+
+        button = self.sender()
+        # key = button.text()
+
+    # 빠 button 클릭
+    def ppaButtonClicked(self):
+        shape = 2
+
+        button = self.sender()
+        # key = button.text()
+
 
 
 if __name__ == '__main__':
